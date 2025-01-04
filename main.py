1bf4068ba7c43bc38706368b2420d4f0dcac7e88
CoolDoge <thedarkguard60@gmail.com>
2025-01-04 20:38:02 +0500

    Переместил получение аргументов запуска в args_loader.py

--- main.py
+++ main.py
@@ -6,58 +6,11 @@ import os
 import time
 import pandas as pd
 import tomllib
-import argparse
-import json
 
+from args_loader import load_args
 from sector import Sector
 from regions_counter import RegionsCounter
 from step_timer import StepTimer
-
-def region_adapt(regions):
-    adapted = {}
-    
-    for idx, region in enumerate(regions):
-        area_number = region['areaNumber']  # TODO: возможно нужно будет использовать
-        coordinates = region['coordinates']
-        
-        # Преобразование к int, так как openCV не берет float
-        points = [[int(coord['x']), int(coord['y'])] for coord in coordinates]
-        
-        # Первый регион - начало, последний - конец, остальные - middle_X
-        if idx == 0:
-            adapted["start"] = points
-        elif idx == len(regions) - 1:
-            adapted["end"] = points
-        else:
-            adapted[f"middle_{idx}"] = points
-
-    return adapted
-
-# Добавление аргументов запуска
-parser = argparse.ArgumentParser()
-parser.add_argument("--video-path", type=str, required=True, help="Путь к видео")
-parser.add_argument("--model-path", type=str, required=True, help="Путь к модельке")
-parser.add_argument("--output-path", type=str, required=True, help="Путь для выходного файлы")
-parser.add_argument("--report-path", type=str, required=True, help="Путь для выходного отчета")
-parser.add_argument("--regions", type=str, required=True, help="Массив точек областей")
-
-# Получение всех аргументов
-args = parser.parse_args()
-
-# Парсинг регионов
-if args.regions:
-    with open(args.regions, 'r') as f:
-        list_region = json.load(f)
-else:
-    list_region = json.loads(args.list_region)
-print(f"Regions: {list_region}")
-
-# Доступ к аргументам
-video_path = args.video_path
-model_path = args.model_path
-output_path = args.output_path
-report_path = args.report_path
-regions = region_adapt(list_region)
 
 def get_fps(cap) -> float|int:
     major_ver, _, _ = cv2.__version__.split('.')
@@ -67,6 +20,8 @@ def get_fps(cap) -> float|int:
 
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+video_path, model_path, output_path, report_path, regions = load_args()
 
 # Подгружаем данные из TOML файла.
 # Надеюсь Гуидо ван Россум простит меня за это.
