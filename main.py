1e7c93bc76080c053c39ede7d4e2d878d3b7a535
CoolDoge <thedarkguard60@gmail.com>
2025-02-15 21:11:22 +0500

    refactor stats report generation, remove os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import cv2
-import os
 import tomllib
 import pandas as pd
 from pandas.io.excel import ExcelWriter
@@ -10,6 +9,40 @@ from sector import SectorCluster
 from regions_counter import RegionsCounter
 from step_timer import StepTimer
 
+def create_stats_report(sector_cluster: SectorCluster, report_path: str):
+    traffic_stats = sector_cluster.traffic_stats()
+    classwise_stats = sector_cluster.classwise_stats()
+    logging.info("Созданы датафреймы со статистикой.")
+
+    res_dataframes = []
+    i = 1
+    for traf_stat, class_stat in zip(traffic_stats, classwise_stats):
+        df_res_tmp = pd.concat([traf_stat, class_stat], axis=1)
+        res_dataframes.append(df_res_tmp)
+
+        print("*********************")
+        print(f"Sector #{i}")
+        print(df_res_tmp)
+        i += 1
+
+    # Запись данных в файл
+    with ExcelWriter(report_path) as writer:
+        for ind, df in enumerate(res_dataframes):
+            df.to_excel(writer, sheet_name=f"{ind + 1}")
+
+    # Освобождаем ресурсы
+    cap.release()
+    output.release()
+    cv2.destroyAllWindows()
+
+    logging.info(f"Видеофайл сохранён в {output_path}")
+
+def get_fps(cap) -> float|int:
+    major_ver, _, _ = cv2.__version__.split('.')
+    if int(major_ver) >= 3:
+        return cap.get(cv2.CAP_PROP_FPS)
+    return cap.get(cv2.cv.CV_CAP_PROP_FPS)
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(levelname)s - %(message)s",
@@ -18,18 +51,8 @@ logging.basicConfig(
     ]
 )
 
-def get_fps(cap) -> float|int:
-    major_ver, _, _ = cv2.__version__.split('.')
-    if int(major_ver) >= 3:
-        return cap.get(cv2.CAP_PROP_FPS)
-    return cap.get(cv2.cv.CV_CAP_PROP_FPS)
-
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-
 video_path, model_path, output_path, report_path, list_region = load_args()
 
-# Подгружаем данные из TOML файла.
-# Надеюсь Гуидо ван Россум простит меня за это.
 with open("settings.toml", "rb") as f:
     settings = tomllib.load(f)
 
@@ -82,23 +105,6 @@ while cap.isOpened():
     sector.update(counter.regions)
     logging.info(f"Обновлены сектора по времени {sector.period_timer.time}")
 
-    # cv2.putText(
-    #     frame,
-    #     f"{sector.sectors[0].classwise_traveled_count}",
-    #     (10, 200),
-    #     cv2.FONT_HERSHEY_SIMPLEX,
-    #     1,
-    #     (255, 255, 255)
-    # )
-    # cv2.putText(
-    #     frame,
-    #     f"Current period timer: {int(sector.period_timer.time)}",
-    #     (10, 230),
-    #     cv2.FONT_HERSHEY_SIMPLEX,
-    #     1,
-    #     (255, 255, 255)
-    # )
-
     # Показ текущего кадра
     output.write(frame)
 
@@ -108,30 +114,5 @@ while cap.isOpened():
 sector.new_period()
 logging.info("Обработка видео завершена.")
 
-# TODO: Максим: сделать генерацию отчета за все периоды
-traffic_stats = sector.traffic_stats()
-classwise_stats = sector.classwise_stats()
-logging.info("Созданы датафреймы со статистикой.")
-
-res_dataframes = []
-i = 1
-for traf_stat, class_stat in zip(traffic_stats, classwise_stats):
-    df_res_tmp = pd.concat([traf_stat, class_stat], axis=1)
-    res_dataframes.append(df_res_tmp)
-
-    print("*********************")
-    print(f"Sector #{i}")
-    print(df_res_tmp)
-    i += 1
-
-# Запись данных в файл
-with ExcelWriter(report_path) as writer:
-    for ind, df in enumerate(res_dataframes):
-        df.to_excel(writer, sheet_name=f"{ind + 1}")
-
-# Освобождаем ресурсы
-cap.release()
-output.release()
-cv2.destroyAllWindows()
-
-logging.info(f"Видеофайл сохранён в {output_path}")
+# Создание отчёта
+create_stats_report(sector, report_path)
