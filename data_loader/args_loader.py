42a6a8a589d03c5694f85cd6dd71efcc3ac4d34f
Ivan <parfenov314ivan@gmail.com>
2025-03-21 09:13:00 -0700

    update args_loader to new sector format (program does not work yet)

--- data_loader/args_loader.py
+++ data_loader/args_loader.py
@@ -9,29 +9,38 @@ def load_args():
     parser.add_argument("--model-path", type=str, required=True, help="Путь к модельке")
     parser.add_argument("--output-path", type=str, required=True, help="Путь для выходного файлы")
     parser.add_argument("--report-path", type=str, required=True, help="Путь для выходного отчета")
-    parser.add_argument("--regions", type=str, required=True, help="Массив точек областей")
+    parser.add_argument("--sector_data", type=str, required=True, help="Массив точек областей")
 
     # Получение всех аргументов
     args = parser.parse_args()
 
     # Парсинг регионов
-    if args.regions:
-        with open(args.regions, 'r') as f:
-            list_region = json.load(f)
-    else:
-        list_region = json.loads(args.list_region)
+    with open(args.sector_data, "r", encoding="utf-8") as file:
+        data = json.load(file)  # вместо json.loads()
 
-    if(len(list_region) % 2 != 0):
-        raise ValueError("Количество регионов не чётное")
+    # Разбираем данные
+    for sector in data["sectors"]:
+        sector_id = sector["sector_id"]
+        region_start = sector["region_start"]["coords"]
+        region_end = sector["region_end"]["coords"]
+        lanes = [lane["coords"] for lane in sector["lanes"]]
+        lanes_count = sector["lanes_count"]
+        
+        print(f"Sector ID: {sector_id}")
+        print(f"Region Start: {region_start}")
+        print(f"Region End: {region_end}")
+        print(f"Lanes: {lanes}")
+        print(f"Lanes Count: {lanes_count}")
+        print("-" * 40)
 
     # Доступ к аргументам
     video_path = args.video_path
     model_path = args.model_path
     output_path = args.output_path
     report_path = args.report_path
-    list_region = list_region
-
-    return video_path, model_path, output_path, report_path, list_region
+    sector_data = sector_data
+    
+    return video_path, model_path, output_path, report_path, sector_data
 
 def get_adapted_region_points(regions, video_width, required_width) -> list[list[int]]:
     coeff = video_width / required_width
