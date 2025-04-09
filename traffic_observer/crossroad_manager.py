94849abe5f9e591b1383014b518466236f9e2b88
Ivan <parfenov314ivan@gmail.com>
2025-04-09 20:47:42 +0500

    upd: add start_delay and travel_time capture

--- traffic_observer/crossroad_manager.py
+++ traffic_observer/crossroad_manager.py
@@ -16,15 +16,18 @@ from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator
 
 class TrackedVehicle:
-    def __init__(self, class_name: str, bbox):
+    def __init__(self, class_name: str, bbox, lane_id: int):
         self.track_class = class_name
         self.bbox = bbox
+        self.lane_id = lane_id
+        self.start_delay = 0.0
+        self.travel_time = 0.0
 
 class Direction:
     def __init__(self, start_lanes, end_regions):
         self.start_lanes = [Region(points) for points in start_lanes]
         self.end_regions = [Region(points) for points in end_regions]
-        self.tracked_vehicled: dict[int, TrackedVehicle] = {} # track_id -> vehicle_id
+        self.tracked_vehicles: dict[int, TrackedVehicle] = {}
 
 class CrossroadManager:
     def __init__(
@@ -50,11 +53,15 @@ class CrossroadManager:
 
     def __annotate_debug(self, annotator, box, track_id, track_class):
         color=(50, 0, 0)
+        #class_name = self.vehicle_classes[int(track_class)]
+        
         label = f'ID {track_id}"'
-
-        if track_id in self.direction.tracked_vehicled:
+        if track_id in self.direction.tracked_vehicles:
             color=(255, 0, 0)
-            label = f'ID {track_id} | Lane: {...}"'
+            lane_id = self.direction.tracked_vehicles[track_id].lane_id
+            start_delay = f"{self.direction.tracked_vehicles[track_id].start_delay:.2f}"
+            travel_time = f"{self.direction.tracked_vehicles[track_id].travel_time:.2f}"
+            label = f'ID {track_id} | Lane: {lane_id} | {start_delay} | {travel_time}"'
 
         annotator.box_label(box, label, color)
 
@@ -67,21 +74,32 @@ class CrossroadManager:
             self.__annotate_debug(annotator, box, track_id, track_class)
 
             # Check tracked ids
-            if track_id in self.direction.tracked_vehicled:
+            if track_id in self.direction.tracked_vehicles:
+                tracked_vehicle = self.direction.tracked_vehicles[track_id]
+                if self.direction.start_lanes[tracked_vehicle.lane_id].is_intersected(box):
+                    tracked_vehicle.start_delay += self.period_timer.step
+                else:
+                    tracked_vehicle.travel_time += self.period_timer.step
+
                 for region in self.direction.end_regions:
                     if region.is_intersected(box):
-                        self.direction.tracked_vehicled.pop(track_id)
+                        # TODO: save vehicle data
+                        self.direction.tracked_vehicles.pop(track_id)
+                continue
 
             # Check untracked ids
             for lane in self.direction.start_lanes:
                 if lane.is_intersected(box):
-                    self.direction.tracked_vehicled[track_id] = TrackedVehicle(track_class, box)
+                    lane_id = self.direction.start_lanes.index(lane)
+                    self.direction.tracked_vehicles[track_id] = TrackedVehicle(track_class, box, lane_id)
         
         # Drawing region borders
         for lane in self.direction.start_lanes:
             lane.draw_regions(frame)
         for lane in self.direction.end_regions:
             lane.draw_regions(frame) # TODO: Change color to red
+
+        logging.info("Frame processed")
 
     def __get_vehicle_travel_time_debug(self, vehicle_id: int) -> float:
         # Get travel time for a vehicle by its ID
